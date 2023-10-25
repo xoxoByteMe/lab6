4183b38a89a79380337988382cc149fb25e0a948
byteMe <estherolatunji101@gmail.com>
2023-10-25 12:24:19 -0400

    main function

---
+++ main.py
@@ -0,0 +1,15 @@
+
+def main():
+    print("Encode Decode Menu")
+    print("-"*18)
+    print("1. Encode \n 2. Decode \n 3. Exit")
+
+def encode():
+    
+    ...
+
+def decode():
+    ...
+
+if __name__ == "__main__":
+    main()
