c669e5b0f45640d9f57d0ac114ec379a156aaf06
byteMe <102075383+xoxoByteMe@users.noreply.github.com>
2023-10-28 19:24:02 -0400

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-
 def main():
-    print("Encode Decode Menu")
-    print("-"*18)
-    print("1. Encode \n 2. Decode \n 3. Exit")
+    option = True
+    while True:
+        print("Encode Decode Menu")
+        print("-"*18)
+        print("1. Encode \n 2. Decode \n 3. Exit")
+        choice = int(input("Select an option:"))
 
-def encode():
-    
-    ...
+        if choice == 1:
+            password = input("Type string of numbers to encode: ")
+            encoder = encode(password)
+            print("Your password has been encoded and stored!")
+        elif choice == 2:
+            decoder = decode(encoder)
+            print(f"The encoded password is is {encoder}, and the original password is {decoder}.")
+        elif choice == 3:
+            break
+        else:
+            print("Invalid choice. Please try again")
+def encode(password):
+    encoder = ""
+    for i in password:
+        password_encode = int(i)
+        password_encode += 3
+        password_encode %= 10
+        encoder += str(password_encode)
+    return encoder
 
 def decode(password_string):   # brenda added decode function
     password = ""
